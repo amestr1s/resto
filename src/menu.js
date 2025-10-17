996365b3ad6d014eb58bf056ce8621f1ab10617c
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-10-17 12:58:19 +0300

    Add menu imgs

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,8 @@
+import mapoImg from "./img/mapo.png";
+import tonkatsuImg from "./img/tonkatsu.png";
+import salmonImg from "./img/salmon.png";
+
+
 function menuPage() {
     const content = document.querySelector("#content");
     const titleDiv = document.createElement("div");
@@ -13,9 +18,15 @@ function menuPage() {
     const mapoH2 = document.createElement("h2");
     mapoH2.textContent = "Mapo Tofu";
     mapoDiv.appendChild(mapoH2);
+    const mapoDivCon = document.createElement("div");
+    mapoDivCon.classList.add("mapoDivCon");
+    mapoDiv.appendChild(mapoDivCon);
     const mapoP = document.createElement("p");
     mapoP.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus fringilla lacus imperdiet scelerisque. Ut blandit orci sed cursus bibendum. Aliquam hendrerit a orci eget mattis. Donec est risus, ullamcorper vel eros ac, vehicula tristique sapien. Vivamus ut nulla justo. Etiam vel commodo sem. ";
-    mapoDiv.appendChild(mapoP);
+    mapoDivCon.appendChild(mapoP);
+    const mapo = document.createElement("img");
+    mapo.src = mapoImg;
+    mapoDivCon.appendChild(mapo);
     //tonkatsu
     const tonkatsuDiv = document.createElement("div");
     tonkatsuDiv.classList.add("tonkatsu");
@@ -23,9 +34,15 @@ function menuPage() {
     const tonkatsuH2 = document.createElement("h2");
     tonkatsuH2.textContent = "Tonkatsu";
     tonkatsuDiv.appendChild(tonkatsuH2);
+    const tonkatsuDivCon = document.createElement("div");
+    tonkatsuDivCon.classList.add("tonkatsuDivCon");
+    tonkatsuDiv.appendChild(tonkatsuDivCon);
     const tonkatsuP = document.createElement("p");
     tonkatsuP.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus fringilla lacus imperdiet scelerisque. Ut blandit orci sed cursus bibendum. Aliquam hendrerit a orci eget mattis. Donec est risus, ullamcorper vel eros ac, vehicula tristique sapien. Vivamus ut nulla justo. Etiam vel commodo sem. ";
-    tonkatsuDiv.appendChild(tonkatsuP);
+    tonkatsuDivCon.appendChild(tonkatsuP);
+    const tonkatsu = document.createElement("img");
+    tonkatsu.src = tonkatsuImg;
+    tonkatsuDivCon.appendChild(tonkatsu);
     //salmon
     const salmonDiv = document.createElement("div");
     salmonDiv.classList.add("salmon");
@@ -33,9 +50,15 @@ function menuPage() {
     const salmonH2 = document.createElement("h2");
     salmonH2.textContent = "Salmon Teriyaki";
     salmonDiv.appendChild(salmonH2);
+    const salmonDivCon = document.createElement("div");
+    salmonDivCon.classList.add("salmonDivCon");
+    salmonDiv.appendChild(salmonDivCon);
     const salmonP = document.createElement("p");
     salmonP.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus fringilla lacus imperdiet scelerisque. Ut blandit orci sed cursus bibendum. Aliquam hendrerit a orci eget mattis. Donec est risus, ullamcorper vel eros ac, vehicula tristique sapien. Vivamus ut nulla justo. Etiam vel commodo sem. ";
-    salmonDiv.appendChild(salmonP);
+    salmonDivCon.appendChild(salmonP);
+    const salmon = document.createElement("img");
+    salmon.src = salmonImg;
+    salmonDivCon.appendChild(salmon);
     return;
 }
 
